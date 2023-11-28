ce49f42897a5a632d92f12458bcd2ca15791bafd
goneal26 <g.oneal26@ncf.edu>
2023-11-28 13:40:44 -0500

    Creating template for viewing posts

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def start():  # put application's code here
-    return render_template('post.html')
+    return render_template('newpost.html')
 
 
 if __name__ == '__main__':
