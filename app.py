e310557e548920a203d0aec26f6f2f1357792e1c
goneal26 <g.oneal26@ncf.edu>
2023-12-01 13:40:19 -0500

    Database hookup

--- app.py
+++ app.py
@@ -1,11 +1,56 @@
-from flask import Flask, render_template
+import sqlite3
+from flask import Flask, render_template, request, redirect
+import random
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def start():  # put application's code here
+def create_post():  # put application's code here
     return render_template('newpost.html')
+
+
+@app.route('/view_post')
+def view_post():
+    # Connect to the database
+    conn = sqlite3.connect('identifier.sqlite')  # Update with your actual database name
+    cursor = conn.cursor()
+
+    # Get a random post from the 'posts' table
+    cursor.execute('SELECT user_id, message FROM posts ORDER BY RANDOM() LIMIT 1')
+    result = cursor.fetchone()
+
+    # Close the connection
+    conn.close()
+
+    # If there are no posts in the database, set default values
+    user_id = "No User"
+    post = "No Post"
+
+    # Update user_id and post if there is a result
+    if result:
+        user_id, post = result
+
+    return render_template('viewpost.html', user_id=user_id, post=post)
+
+
+@app.route('/submit_post', methods=['POST'])
+def submit_post():
+    message = request.form['post_content']
+    user_id = 1  # Replace with the actual user_id based on your authentication system
+
+    # Connect to the database
+    conn = sqlite3.connect('identifier.sqlite')
+    cursor = conn.cursor()
+
+    # Insert the new post into the 'posts' table
+    cursor.execute('INSERT INTO posts (message, user_id) VALUES (?, ?)', (message, user_id))
+
+    # Commit the changes and close the connection
+    conn.commit()
+    conn.close()
+
+    return redirect('/view_post')  # Redirect to the home page or any other page after submitting the post
 
 
 if __name__ == '__main__':
